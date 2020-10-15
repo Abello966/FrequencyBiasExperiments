4ed9b5b9fb978304587e899b03ed7cfaba70b077
Antonio Augusto Abello (MSc | hirata) <abello@.ime.usp.br>
2020-10-15 00:32:58 -0300

    CIFAR10 ResNet

--- arch.py
+++ arch.py
@@ -6,6 +6,7 @@ from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.applications.vgg19 import VGG19
 
+from tensorflow.keras.regularizers import l2
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.layers import ReLU, Lambda, Dropout, Add, GlobalAveragePooling2D
 from tensorflow.keras import Input, Model
@@ -13,7 +14,7 @@ from tensorflow.keras.layers import BatchNormalization, LayerNormalization
 
 NoNormalization = lambda: Lambda(lambda x: x)
 
-AVAILABLE_ARCHS = ["SmolAlexNet", "AlexNet", "VGG16", "VGG19", "SmolResNet50", "ResNet50", "InceptionResNetV2"]
+AVAILABLE_ARCHS = ["SmolAlexNet", "AlexNet", "VGG16", "VGG19", "CifarResNet", "ResNet50", "InceptionResNetV2"]
 
 def SmolAlexNet(input_tensor=None, classes=1000, Normalization=NoNormalization):
     x = Conv2D(96, 11, padding="valid", activation="relu")(input_tensor)
@@ -34,9 +35,9 @@ def SmolAlexNet(input_tensor=None, classes=1000, Normalization=NoNormalization):
 
     x = Flatten()(x)
     x = Dense(4096, activation="relu")(x)
-    x = Dropout(0.4)(x)
+    x = Dropout(0.5)(x)
     x = Dense(4096, activation="relu")(x)
-    x = Dropout(0.4)(x)
+    x = Dropout(0.5)(x)
     x = Dense(classes, activation="softmax")(x)
 
     return Model(inputs=input_tensor, outputs=x)
@@ -106,33 +107,37 @@ def ResBlockB(x, num, norm_layer):
 
     return x
 
-def LocalResNet50(input_tensor=None, classes=1000, Normalization=BatchNormalization):
-    x = ZeroPadding2D(3)(input_tensor)
-    x = Conv2D(64, 7, strides=2, padding="valid")(input_tensor)
+def CifarResNetBlock(x, nfilters, norm_layer):
+    conv1 = Conv2D(nfilters, 3, padding="same", kernel_regularizer=l2(1e-4))(x)
+    conv1 = norm_layer()(conv1)
+    conv1 = ReLU()(conv1)
+
+    conv2 = Conv2D(nfilters, 3, padding="same", kernel_regularizer=l2(1e-4))(conv1)
+    conv2 = norm_layer()(conv2)
+    conv2 = ReLU()(conv2)
+    
+    x = Add()([conv2, x])
+    x = ReLU()(x)
+    return x
+   
+def CifarResNet(n, input_tensor=None, classes=1000, Normalization=BatchNormalization):
+    x = Conv2D(16, 3, padding="same", kernel_regularizer=l2(1e-4))(input_tensor)
     x = Normalization()(x)
     x = ReLU()(x)
 
-    x = ZeroPadding2D(1)(x)
-    x = MaxPooling2D()(x)
+    for i in range(2 * n):
+        x = CifarResNetBlock(x, 16, Normalization)
 
-    x = ResBlockA(x, 1, 1, Normalization)
-    for i in range(2):
-        x = ResBlockB(x, 1, Normalization)
+    x = Conv2D(32, 3, strides=2, padding="valid", kernel_regularizer=l2(1e-4))(x)
+    for i in range(n - 1):
+        x = CifarResNetBlock(x, 32, Normalization)
 
-    x = ResBlockA(x, 2, 2, Normalization)
-    for i in range(3):
-        x = ResBlockB(x, 2, Normalization)
-
-    x = ResBlockA(x, 4, 2, Normalization)
-    for i in range(5):
-        x = ResBlockB(x, 4, Normalization)
-
-    x = ResBlockA(x, 8, 2, Normalization)
-    for i in range(2):
-        x = ResBlockB(x, 8, Normalization)
+    x = Conv2D(64, 3, strides=2, padding="valid", kernel_regularizer=l2(1e-4))(x)
+    for i in range(n - 1):
+        x = CifarResNetBlock(x, 64, Normalization)
 
     x = GlobalAveragePooling2D()(x)
-    x = Dense(classes, activation="softmax")(x)
+    x = Dense(classes, activation="softmax", kernel_regularizer=l2(1e-4))(x)
     return Model(inputs=input_tensor, outputs=x)
     
 def SmolResNet50(input_tensor=None, classes=1000, Normalization=BatchNormalization):
@@ -187,11 +192,11 @@ def get_arch(arg, input_shape, classes, **kwargs):
     elif arg == "VGG19":
         return VGG19(input_tensor=input_tensor, classes=classes, weights=None, **kwargs)
     elif arg == "ResNet50":
-        return ResNet50(input_tensor=input_tensor, classes=classes, weights=None, **kwargs)
-    elif arg == "SmolResNet50":
-        return SmolResNet50(input_tensor=input_tensor, classes=classes)
+        return LocalResNet50(input_tensor=input_tensor, classes=classes, weights=None, **kwargs)
+    elif arg == "CifarResNet":
+        return CifarResNet(5, input_tensor=input_tensor, classes=classes)
     elif arg == "InceptionResNetV2":
         return InceptionResNetV2(input_tensor=input_tensor, classes=classes, weights=None, **kwargs)
-    elif arg not in AVAILABLE_DATASETS:
+    else:
         show_available()
         raise Exception(arg + " not an available architecture")
